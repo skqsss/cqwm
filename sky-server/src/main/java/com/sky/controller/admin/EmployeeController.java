a4ef3db815fd7c774475d9d7fdd12855d4265aec
sqksss <3571557933@qq.com>
2026-02-11 19:27:48 +0800

    feat(employee): 添加员工新增功能
    - 在EmployeeController中添加新增员工接口
    - 在EmployeeMapper中添加插入员工数据的SQL映射
    - 在EmployeeService及其实现类中添加新增员工业务逻辑
    - 实现员工数据的默认状态、密码设置和时间戳管理
    - 添加BaseContext工具类用于线程内数据传递
    - 在全局异常处理器中添加SQL完整性约束异常处理
    - 添加员工已存在的错误消息常量定义

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -9,7 +10,6 @@ import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -77,4 +77,18 @@ public class EmployeeController {
         return Result.success();
     }
 
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation(value = "新增员工")
+    public Result save(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("新增员工，员工数据：{}", employeeDTO);
+        System.out.println("当前线程的id:" + Thread.currentThread().getId());
+        employeeService.save(employeeDTO);
+        return Result.success();
+    }
 }
