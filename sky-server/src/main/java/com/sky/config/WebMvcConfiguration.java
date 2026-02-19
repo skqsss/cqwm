2496a4129ad4c616c7cfc906e69f12e29556d16e
sqksss <3571557933@qq.com>
2026-02-19 17:42:10 +0800

    feat(admin): 添加菜品管理和文件上传功能
    - 新增CommonController实现图片文件上传功能，支持jpg和png格式
    - 添加DishController提供菜品新增接口
    - 创建DishFlavorMapper和对应XML实现批量插入菜品口味数据
    - 扩展DishMapper添加菜品数据插入方法
    - 实现DishService和DishServiceImpl完成菜品及口味数据保存
    - 配置静态资源映射支持上传文件访问
    - 更新.gitignore文件忽略图片文件

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -74,6 +73,8 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         log.info("开始进行静态资源映射...");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("file:D:/Leaning Project/cqwm/sky-take-out/sky-server/src/main/resources/upload/");
     }
 
     /**
