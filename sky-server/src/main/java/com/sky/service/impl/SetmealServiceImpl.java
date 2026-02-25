7b1d3ae23b942df7f0efeb543399f0636662d84e
sqksss <3571557933@qq.com>
2026-02-25 16:31:54 +0800

    feat(setmeal): 添加套餐分页查询功能
    - 在SetmealController中新增/page接口实现分页查询
    - 在SetmealMapper中添加pageQuery方法和对应的SQL映射
    - 在SetmealService和SetmealServiceImpl中实现分页查询逻辑
    - 集成PageHelper分页插件支持
    - 添加SetmealPageQueryDTO和SetmealVO数据传输对象
    - 实现按名称、状态、分类ID条件筛选功能

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,12 +1,17 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,5 +57,18 @@ public class SetmealServiceImpl implements SetmealService {
         //保存套餐和菜品到套餐关系表setmeal_dish
         setmealDishMapper.insertBatch(setmealDishes);
 
+    }
+
+    /**
+     * 套餐分页查询
+     *
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        PageHelper.startPage(setmealPageQueryDTO.getPage(), setmealPageQueryDTO.getPageSize());
+        Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+        return new PageResult(page.getTotal(), page.getResult());
     }
 }
